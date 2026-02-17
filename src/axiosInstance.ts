925c386e576fca021d2f62d5f4cfda057f85e1f2
exdrama23 <alecviniciusflamengo@gmail.com>
2026-02-17 03:36:03 -0300

    chore: usar backend do render

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // * Configurando axios
 const api = axios.create();
 api.defaults.withCredentials = true;
-api.defaults.baseURL =  window.location.hostname.includes('localhost') ? 'http://localhost:2923/api' : 'https://wealthy-courtney-sabor-a-vida-f6291b31.koyeb.app/api';
+api.defaults.baseURL =  window.location.hostname.includes('localhost') ? 'http://localhost:2923/api' : 'https://sabor-a-vida.onrender.com/api';
 
 api.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem('accessToken') || '';
